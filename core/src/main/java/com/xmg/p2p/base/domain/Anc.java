adf1e257cdc569662bf9744f4b7f123ecf502a22
a18575759226 <31117061+a18575759226@users.noreply.github.com>
2017-08-18 19:27:29 +0800

    上传实体类
    上传实体类cat

---
+++ core/src/main/java/com/xmg/p2p/base/domain/Anc.java
@@ -0,0 +1,14 @@
+package com.xmg.p2p.base.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by liu on 2017/8/18.
+ */
+@Getter@Setter
+public class Anc {
+    private Long id;
+    private  String name;
+    private int state;
+}
---
+++ core/src/main/java/com/xmg/p2p/base/domain/Cat.java
@@ -0,0 +1,15 @@
+package com.xmg.p2p.base.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by liu on 2017/8/18.
+ */
+@Getter@Setter
+public class Cat {
+    private Long id;
+    private int state;
+
+
+}
